b7602bfc479330781fc8be9208ed20ca893cc5f5
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2024-06-25 15:15:01 +0500

    added solidity code and test cases

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,6 @@
+require("@nomicfoundation/hardhat-toolbox");
+
 /** @type import('hardhat/config').HardhatUserConfig */
-
-require("@nomiclabs/hardhat-waffle");
-
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.17",
 };
